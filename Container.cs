f7823f9ab09483966708895890aeea298c42a176
Florencia Parraguez <florenciaparraguezsepulveda@gmail.com>
2022-05-16 10:14:16 -0400

    1

---
+++ Container.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EVA1
+
+	internal class container
+{
+
+}
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
---
+++ ListaBuques.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVA1
+{
+	internal class ListaBuques
+    {
+		Buque[] misBuques = new Buque[30];
+		private bool hayEspacio;
+
+		public ListaBuques()
+        {
+			hayEspacio = true;
+        }
+    }
+}
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
